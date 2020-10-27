e718c565f5671d1e59cd88db1b22febaff413833
ShailySarker <50097463+ShailySarker@users.noreply.github.com>
2020-10-27 16:52:38 +0600

    Add files via upload

---
+++ PHPformvalidation.php
@@ -0,0 +1,40 @@
+<html>
+	<head>
+		<title>PHP form validation</title>
+
+	</head>
+	<body>
+		<hr>
+		<form>
+			<fieldset>
+				<legend><h1>Club Member Registration</h1></legend>
+				<table>
+					<tr>
+						<td align="right">Name:</td>
+						<td><input type="text">&nbsp;&nbsp;</td>
+					</tr>
+					<tr>
+						<td align="right">Username:</td>
+						<td><input type="text"></td>
+					</tr>
+						<tr>
+						<td align="right">Password:</td>
+						<td><input type="password"></td>
+					</tr>
+					<tr>
+						<td align="right">Confirm Password:</td>
+						<td><input type="password"></td>
+					</tr>
+					<tr>
+						<td align="right">Email:</td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td align="right">Name:</td>
+						<td><input type="text">&nbsp;&nbsp;</td>
+					</tr>					
+				</table>
+				
+		</form>
+	</body>
+</html>
