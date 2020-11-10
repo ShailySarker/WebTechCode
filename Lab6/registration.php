4218e9c35148f1b7d91fb92f208592c18810bd2a
ShailySarker <50097463+ShailySarker@users.noreply.github.com>
2020-11-10 16:55:22 +0600

    validation

--- Lab6/registration.php
+++ Lab6/registration.php
@@ -1,3 +1,154 @@
+<?php 
+    $fullname="";
+    $err_fullname="";
+    $username="";
+    $err_username="";
+    $password="";
+    $err_password="";
+    $confirmpassword="";
+    $err_confirmpassword="";
+	$gender="";
+    $err_gender="";
+    $email="";
+    $err_email="";
+    $contact="";
+    $err_contact="";
+	$city="";
+	$err_city="";
+
+    $has_err=false;
+    if(isset($_POST["register"])){
+       
+
+	   ///NAME VALIDATION
+        if(empty($_POST["fullname"])){
+            $err_name="Full Name Required";
+            $has_err=true;
+        }
+        else{
+            $name=htmlspecialchars($_POST["fullname"]);
+        }
+		
+		
+		
+        ///USER NAME VALIDATION
+        if(empty($_POST["username"])){
+            $err_username="Username Required";
+            $has_err=true;
+        }
+        elseif((strlen($_POST["username"])<6)){
+            $err_uname="Username must be 6 char";
+            $has_err=true;
+        }
+        elseif(strpos($_POST["username"]," ")){
+            $err_uname="No space allow in username!";
+            $has_err=true;
+        }
+        else{
+            $uname=htmlspecialchars($_POST["username"]);
+        }
+		
+		
+        ///PASSWORD VALIDATION
+        if(empty($_POST["password"])){
+            $err_pass="Password Required";
+            $has_err=true;
+        }
+        elseif(strlen($_POST["password"])<8){
+            $err_pass="Password must be 8 char";
+            $has_err=true;
+        }
+        elseif(!strpos($_POST["password"],"#") || !strpos($_POST["pass"],"?")){
+            $err_pass="Password must be contained '#' or '?'.";
+            $has_err=true;
+        }
+        elseif(!strpos($_POST["password"],"1")){
+            $err_pass="Password must contain 1 numeric";
+            $has_err=true;
+        }
+        elseif(strtolower($_POST["password"])==$_POST["password"] || strtolower($_POST["password"])==$_POST["password"]){
+            $err_pass="Password must contain Uppercase and Lowercase letter.";
+            $has_err=true;
+        }
+        else{
+            $pass=htmlspecialchars($_POST["password"]);
+        }
+		
+		
+        ///CONFIRMPASSWORD VALIDATION
+        if(empty($_POST["confirmpassword"])){
+            $err_confirmpassword="ConfirmPassword Required";
+            $has_err=true;
+        }
+        elseif(!strcmp($_POST["confirmpassword"],strtoupper($_POST["password"]))){
+            $err_confirmpassword="Password and Confirm Password must be same.";
+            $has_err=true;
+        }
+		
+		
+        ///GENDER VALIDATION
+		
+        if(!isset($_POST["gender"])){
+			$err_gender="Gender Required.";
+			$has_err=true;
+        }
+        else{
+            $gender=htmlspecialchars($_POST["gender"]);
+        }
+		
+		
+		///EMAIL ADDREESS VALIDATION
+        if(empty($_POST["email"])){
+            $err_email="Email Required";
+            $has_err=true;
+        }
+        elseif(strpos($_POST["email"],"@") && strpos($_POST["email"],"."))
+		{
+            if(strpos($_POST["email"],"@") < strpos($_POST["email"],".")){
+                $email=htmlspecialchars($_POST["email"]);
+            }
+            else{
+                $err_email="'@' Must be before '.'.";
+                $has_err=true;
+            }
+        }
+        else{
+            $err_email="Email must contain '@' and '.'.";
+            $has_err=true;
+        }
+		
+		
+        ///CONTACT NO VALIDATION
+        if(empty($_POST["contact"]))
+		{
+			$err_contact = "Contact no. required";
+		}
+		else if(!is_numeric($_POST["contact"]))
+		{
+			$err_contact = "Numeric charecter required";
+		}
+		else{ $number = htmlspecialchars($_POST["contact"]);
+		}
+		
+		
+		///CITY VALIDATION
+		if(isset($_POST["city"])){
+            $city=htmlspecialchars($_POST["city"]);
+        }
+        else{
+            $err_city="Please Select city";
+            $has_err=true;
+        }
+		
+		
+		
+	}
+
+?>
+
+
+
+
 <html>
 	<head>
 		<title>Mid Lab Task</title>
