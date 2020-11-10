f1991eb960ff238da331c1dd07e3a14dd1dd8752
ShailySarker <50097463+ShailySarker@users.noreply.github.com>
2020-11-10 18:49:12 +0600

    okk

--- Lab6/registration.php
+++ Lab6/registration.php
@@ -1,154 +1,6 @@
-<?php 
-    $fullname="";
-    $err_fullname="";
-    $username="";
-    $err_username="";
-    $password="";
-    $err_password="";
-    $confirmpassword="";
-    $err_confirmpassword="";
-	$gender="";
-    $err_gender="";
-    $email="";
-    $err_email="";
-    $contact="";
-    $err_contact="";
-	$city="";
-	$err_city="";
-
-    $has_err=false;
-    if(isset($_POST["register"])){
-       
-
-	   ///NAME VALIDATION
-        if(empty($_POST["fullname"])){
-            $err_name="Full Name Required";
-            $has_err=true;
-        }
-        else{
-            $name=htmlspecialchars($_POST["fullname"]);
-        }
-		
-		
-		
-        ///USER NAME VALIDATION
-        if(empty($_POST["username"])){
-            $err_username="Username Required";
-            $has_err=true;
-        }
-        elseif((strlen($_POST["username"])<6)){
-            $err_uname="Username must be 6 char";
-            $has_err=true;
-        }
-        elseif(strpos($_POST["username"]," ")){
-            $err_uname="No space allow in username!";
-            $has_err=true;
-        }
-        else{
-            $uname=htmlspecialchars($_POST["username"]);
-        }
-		
-		
-        ///PASSWORD VALIDATION
-        if(empty($_POST["password"])){
-            $err_pass="Password Required";
-            $has_err=true;
-        }
-        elseif(strlen($_POST["password"])<8){
-            $err_pass="Password must be 8 char";
-            $has_err=true;
-        }
-        elseif(!strpos($_POST["password"],"#") || !strpos($_POST["pass"],"?")){
-            $err_pass="Password must be contained '#' or '?'.";
-            $has_err=true;
-        }
-        elseif(!strpos($_POST["password"],"1")){
-            $err_pass="Password must contain 1 numeric";
-            $has_err=true;
-        }
-        elseif(strtolower($_POST["password"])==$_POST["password"] || strtolower($_POST["password"])==$_POST["password"]){
-            $err_pass="Password must contain Uppercase and Lowercase letter.";
-            $has_err=true;
-        }
-        else{
-            $pass=htmlspecialchars($_POST["password"]);
-        }
-		
-		
-        ///CONFIRMPASSWORD VALIDATION
-        if(empty($_POST["confirmpassword"])){
-            $err_confirmpassword="ConfirmPassword Required";
-            $has_err=true;
-        }
-        elseif(!strcmp($_POST["confirmpassword"],strtoupper($_POST["password"]))){
-            $err_confirmpassword="Password and Confirm Password must be same.";
-            $has_err=true;
-        }
-		
-		
-        ///GENDER VALIDATION
-		
-        if(!isset($_POST["gender"])){
-			$err_gender="Gender Required.";
-			$has_err=true;
-        }
-        else{
-            $gender=htmlspecialchars($_POST["gender"]);
-        }
-		
-		
-		///EMAIL ADDREESS VALIDATION
-        if(empty($_POST["email"])){
-            $err_email="Email Required";
-            $has_err=true;
-        }
-        elseif(strpos($_POST["email"],"@") && strpos($_POST["email"],"."))
-		{
-            if(strpos($_POST["email"],"@") < strpos($_POST["email"],".")){
-                $email=htmlspecialchars($_POST["email"]);
-            }
-            else{
-                $err_email="'@' Must be before '.'.";
-                $has_err=true;
-            }
-        }
-        else{
-            $err_email="Email must contain '@' and '.'.";
-            $has_err=true;
-        }
-		
-		
-        ///CONTACT NO VALIDATION
-        if(empty($_POST["contact"]))
-		{
-			$err_contact = "Contact no. required";
-		}
-		else if(!is_numeric($_POST["contact"]))
-		{
-			$err_contact = "Numeric charecter required";
-		}
-		else{ $number = htmlspecialchars($_POST["contact"]);
-		}
-		
-		
-		///CITY VALIDATION
-		if(isset($_POST["city"])){
-            $city=htmlspecialchars($_POST["city"]);
-        }
-        else{
-            $err_city="Please Select city";
-            $has_err=true;
-        }
-		
-		
-		
-	}
-
+<?php
+	include_once "allvalidation/registration_validation.php";
 ?>
-
-
-
-
 <html>
 	<head>
 		<title>Mid Lab Task</title>
@@ -157,55 +9,55 @@
 		<center>
 		<fieldset>
 		
-			<form>
+			<form action="" method="post">
 				<h1> Welcome to Registeration.</h1>
 				<table>
 				
 					<tr>
 						<td>Full Name:</td>
 						<td><input type="text" value="<?php echo $fullname?>" name="fullname"></td>
+						<td><span style="color:red;"><?php echo $err_fullname;?></span>
 					</tr>
 					<tr>
 						<td>User Name:</td>
 						<td><input type="text" value="<?php echo $username?>" name="username"></td>
+						<td><span style="color:red;"><?php echo $err_username;?></span>
 					</tr>
 					<tr>					
 						<td>Password:</td>
 						<td><input type="text" value="<?php echo $password?>" name="password"></td>
+						<td><span style="color:red;"><?php echo $err_password;;?></span>
 					</tr>
 					<tr>					
 						<td>Confirm Password:</td>
-						<td><input type="text" value="<?php echo $confirmpassword?>" name="confirmpassword"></td>
+						<td><input type="text" value="<?php echo $confirmpassword?>"name="confirmpassword"></td>
+						<td><span style="color:red;"><?php echo $err_confirmpassword;;?></span>
 					</tr>
 					<tr>
 						<td>Gender:</td>
 						<td>
 							<input type="radio" name="gender" value="Male"> Male
-							<input type="radio" name="gender" value="Female"> Female   <?php echo " ".$err_gender; ?>
+							<input type="radio" name="gender" value="Female"> Female   
 						</td>
+						<td><span style="color:red;"><?php echo $err_gender;?></span>
 					</tr>	
 					<tr>	
 						<td>E-mail Address:</td>
-						<td><input type="text" value="<?php echo $email?>" name="email"></td>					
+						<td><input type="text" value="<?php echo $email?>" name="email"></td>
+						<td><span style="color:red;"><?php echo $err_email;?></span>						
 					</tr>
 					<tr>
 						<td>Contact No:</td>
 						<td><input type="text" value="<?php echo $contact?>" name="contact"></td>
+						<td><span style="color:red;"><?php echo $err_contact;?></span>
 					</tr>
-					<tr>
-						<td></td>
-						<td colspan="2" align="rigth">
-							<input type="submit" name="ok" value="Ok">
-						</td>
-						
-					</tr>
-					
+				
 					<tr>
 						<td>City:</td>
 						<td>
 							<select name="city" >
 								<?php
-									$city = array("Dhaka","Khulna","Barishal","Rongpur");
+									$city = array("Dhaka","Khulna","Barishal","Rangpur");
 									for ($i = 0; $i < 3; $i++) {
 										if($i == 0)
 										{
@@ -220,7 +72,15 @@
 								?>						
 							</select>
 						</td>
-					</tr>	
+						<td><span style="color:red;"><?php echo $err_city;?></span>
+					</tr>
+					<tr>
+						<td></td>
+						<td colspan="2" align="center">
+							<input type="submit" name="Ok" value="Ok">
+						</td>
+						
+					</tr>					
 				</table>
 			
 			</form>
