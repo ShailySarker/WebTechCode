33f81427b75cb4f1f6b6528ef1f4bb69b12266d4
ShailySarker <50097463+ShailySarker@users.noreply.github.com>
2020-11-10 20:20:31 +0600

    okk

---
+++ Lab6/login.php
@@ -0,0 +1,36 @@
+<?php
+	include_once "allvalidation/login_validation";
+?>
+
+
+<html>
+	<head></head>
+	<body>
+		<fieldset>
+			<form action="" method="post">
+				<table>
+					<tr>
+						<td>Username:</td>
+					</tr>
+					<tr>					
+						<td><input type="text" value="<?php echo $username?>" name="username"></td>
+						<td><?php echo $err_username;?>
+						</td>
+					</tr>
+					<tr>
+						<td>Password:</td>
+					</tr>
+					<tr>	
+						<td><input type="password" value="<?php echo $password?>" name="password"></td>
+						<td><?php echo $err_password;?>
+						</td>
+				
+						<td colspan="2" align="right">
+							<input type="submit" name="login" value="Login">
+						</td>
+					</tr>
+				</table>
+			</form>
+		</fieldset>
+	</body>
+</html>
