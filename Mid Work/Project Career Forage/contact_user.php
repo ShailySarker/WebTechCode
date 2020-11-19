24353e94ad7f516fe3e531063e1e0ad6eb4f3a47
ShailySarker <50097463+ShailySarker@users.noreply.github.com>
2020-11-19 22:49:48 +0600

    done

---
+++ Mid Work/Project Career Forage/contact_user.php
@@ -0,0 +1,78 @@
+<html>
+	<center>
+	<fieldset style="background-color:powderblue;height:70%;width:40%">
+	<head>
+	<fieldset>
+		<span style="color: blue"><h1>Career Forage</h1></span>
+	</fieldset>
+	</head>
+	<body>
+		<fieldset>
+			
+			<form action="" method="post">
+				<table>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr><td><h2 align="center">Is the registered user accepted particular circular??</h2></td></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr>
+				<tr>
+					<td align="center"><button type="submit" style="color: Blue;height:30px;width:250px" formaction="contact_user_accepted.php"><strong>Accepted</strong></button></td>
+				</tr>
+				<tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr>
+				<tr>
+					<td align="center"><button type="submit" style="color: red;height:30px;width:250px" formaction="dashboard_moderator.php"><strong>Not Accepted</strong></button></td>
+				</tr>
+				<tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr>
+				<tr></tr>
+				<tr></tr><tr></tr>
+				<tr><td align="center"><button type="submit"  name="back"formaction="dashboard_moderator.php"><strong>Back<strong></button></td></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr><tr></tr>
+				<tr></tr>
+			</form>
+		</fieldset>
+	</body>
+	</fieldset>
+	</center>
+</html>	
---
+++ Mid Work/Project Career Forage/contact_user_accepted.php
@@ -0,0 +1,78 @@
+<html>
+	<center>
+	<fieldset style="background-color:powderblue;height:80%;width:70%">
+	<fieldset>
+	
+	<head><h1 style="color:blue"> Career Forage</h1>
+	<style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 10px;
+        }
+
+        
+    </style>
+	</head>
+	
+	</fieldset>
+	<fieldset>
+	<center>
+	<body>
+		<h1>The Users are Accepted Particular Circular</h1>
+		<table>
+		
+			<tr>
+				<td align="left"> <a href="dashboard_moderator.php">Go to Moderator Homepage</a> </td>
+			</tr>
+			<tr>
+					<th>Serial no</th>
+					<th>User Name</th>
+					<th>Email Address</th>
+					<th>Contact no</th>
+					<th><table><tr>
+						Address
+						<td>Street</td>
+						<td>City</td>
+						<td>State</td>
+						<td>Pastal/Zip</td>
+						</tr></table>
+					</th>
+					
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>Anindya</td>
+				<td>anindya123@gmail.com</td>
+				<td>012344567</td>
+				<td>
+					123,
+					Dhaka,
+					South,
+					1229		
+				</td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>AdibAdib</td>
+				<td>adib123@gmail.com</td>
+				<td>012344567</td>
+				<td>
+					23,
+					Khulna,
+					East,
+					1209
+					
+				</td>
+			</tr>	
+			
+			
+		</table>
+	</body>
+	</center>
+	</fieldset>
+	</fieldset>
+	</center>
+</html>		
