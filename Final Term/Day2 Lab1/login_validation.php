c39b8f77ca75898657a061435bf3670ecbd4b564
ShailySarker <50097463+ShailySarker@users.noreply.github.com>
2020-11-24 19:02:59 +0600

    done

---
+++ Final Term/Day2 Lab1/login_validation.php
@@ -0,0 +1,93 @@
+<?php
+	session_start();
+	$username="";
+	$err_username="";
+	$password="";
+	$err_password="";
+	$hasError=false;
+	$dbservername="localhost";
+	$dbusername="root";
+	$dbpassword="";
+	$dbname="Day1";
+	
+	$hasError = false;
+	
+	if(isset($_POST["login"])){
+		
+		///username validation
+		if(empty($_POST["username"])){
+			$err_username="*Username Required";
+			$hasError =true;	
+		}
+		else{
+			$username = htmlspecialchars($_POST["username"]);
+		}
+		
+		///password validation
+		if(!empty($_POST["password"]))
+		{
+			if(strlen($_POST["password"]) >= 8)
+			{
+				if(!(strtolower($_POST["password"]) == $_POST["password"]) && (!(strtoupper($_POST["password"]) == $_POST["password"])))
+				{
+					$hasNumber = false;
+					$num_arr = array("0","1","2","3","4","5","6","7","8","9");
+					$pass =htmlspecialchars($_POST["password"]);
+					for($i = 0; $i < strlen($pass); $i++)
+					{
+						for($j = 0; $j <10; $j++)
+						{
+							if($pass[$i]== $num_arr[$j])
+							{
+								//echo "yo<br>";
+								$hasNumber = true;
+								break;
+							}
+						}
+					}
+					if($hasNumber == true)
+					{
+						if(strpos($_POST["password"], "#") || strpos($_POST["password"], "?"))
+						{
+							$password = htmlspecialchars($_POST["password"]);
+						}
+						else{
+							$err_password = "*atleast one special character # or ? is required";
+							$hasError = true;
+							}
+					}
+					else{
+						$err_password = "*atleast one digit is required";
+						$hasError = true;
+						}
+				}
+				else{
+					$err_password = "*upper and lower case character required";
+					$hasError = true;
+					}
+			}
+			else{
+				$err_password = "*minimum password length is 8";
+				$hasError = true;
+				}
+		}
+		else{
+			$err_password = "*Password required";
+			$hasError = true;
+			}
+
+		
+		$encodpassword=md5($password);
+		$conn=mysqli_connect($dbservername,$dbusername,$dbpassword,$dbname);
+		$q = "select * from users where userName='$username' and password='$encodpassword'";
+		if(!$hasError){
+			$result =mysqli_query($conn, $q);
+			if(mysqli_num_rows($result)>0)
+			{
+				header("Location: dashboard.php");
+				echo "<h1>login Successfully</h1> ";
+			}
+		}
+	}
+	
+?>
---
+++ Final Term/Day2 Lab1/registation.php
@@ -0,0 +1,97 @@
+<?php
+	
+	include_once "registration_validation.php";
+?>
+
+	
+<html>
+	<head>
+		<title>Registation</title>
+	</head>
+	<body>
+	<html>
+	<head></head>
+	<body><h1 align="center">Welcome to Registation....</h1>
+		<center>
+		<fieldset style="background-color:powderblue;height:50%;width:40%">
+		* Fields are required
+			<form action="" method="post">
+				<table><h1 align="center">Please fill up the form....</h1>
+					
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr><tr></tr><tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr><tr></tr><tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr><tr></tr><tr></tr><tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr><tr></tr><tr></tr><tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr><tr></tr><tr></tr>
+					<tr>
+						<td align="right"><strong>User Name:</strong></td>
+						<td><input type="text" placeholder="* Write your username" value="<?php echo $username?>" name="username"></td>
+						<td><span style="color:red;"><?php echo $err_username;?></span>
+						</td>
+					</tr>
+					
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr><tr></tr><tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr><tr></tr><tr></tr>
+					<tr>
+						<td align="right"><strong>Password:</strong></td>
+						<td><input type="password" placeholder="* Enter your password" value="<?php echo $password?>" name="password"></td>
+						<td><span style="color:red;"><?php echo $err_password;?></span>
+						</td>
+					</tr>
+					
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr><tr></tr><tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr><tr></tr><tr></tr>
+					<tr></tr><tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr>
+					<tr></tr><tr></tr><tr></tr>
+					<tr>
+						<td colspan="2" align="right">
+							<input type="submit" style="color:Blue" name="register" value="register">
+						</td>
+					</tr>
+				</table>
+			</form>
+		</fieldset>
+		</center>
+	</body>
+</html>
+	</body>
+	
+</html>
