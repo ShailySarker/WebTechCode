24f4c78a5d0adef380c6467ac7c663a72b47f01c
ShailySarker <50097463+ShailySarker@users.noreply.github.com>
2020-12-20 22:42:04 +0600

    done

--- Final Term/Quiz2/MV/Controller/Book_Controller.php
+++ Final Term/Quiz2/MV/Controller/Book_Controller.php
@@ -1,1 +1,25 @@
+<?php
 
+require_once "../Model/Books_Database_Connection.php";
+
+function getAllBook()
+{
+    $queryAllBook = "select * from books";
+    $data = getValues($queryAllBook);
+    return $data;
+}
+
+function getBookDetail($id)
+{
+    $queryBookDetail = "select * from books where ID = '" . $id . "';";
+    $data = getValues($queryBookDetail);
+    return $data;
+}
+
+
+function getSearchBook($BookName)
+{
+    $querySearchBook = "SELECT * FROM books WHERE Name like '%" . $BookName . "%';";
+    $data = getValues($querySearchBook);
+    return $data;
+}
